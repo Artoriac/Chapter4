52c1d2a9e7281dfde87039c4af0ab500f613db1c
Yaroslav <91912469+Artoriac@users.noreply.github.com>
2023-08-24 13:46:58 +0300

    Stepik4_2_8
    Изменения связаны с добавлением перехода между страницами. Закоменченный код не убирал. Оставил второй метод

--- test_main_page.py
+++ test_main_page.py
@@ -5,9 +5,10 @@ from pages.login_page import LoginPage
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # Выполняем метод страницы - переходим на станицу логина
-
+    page.open()
+    page.go_to_login_page()# открываем страницу
+    login_page = LoginPage(browser, browser.current_url)  # Выполняем метод страницы - переходим на станицу логина
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -28,6 +29,8 @@ def test_guest_should_be_login_form(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
+
+
 def test_guest_should_be_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
