44cee2a15656a1bd5696f5f9631de637cb8429ed
Yaroslav <91912469+Artoriac@users.noreply.github.com>
2023-09-04 17:06:54 +0300

    Финальные штрихи
    Закончил подготовку своего проекта для сдачи финальной задачи в курсе

--- test_main_page.py
+++ test_main_page.py
@@ -52,16 +52,6 @@ def test_gust_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page.should_be_empty_basket_without_items()
     basket_page.should_be_empty_basket_check_with_text()
 
-
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_empty_basket_without_items()
-    basket_page.should_be_empty_basket_check_with_text()
-
 # @pytest.mark.login - Работа с API выходит за рамки этого курса
 # class TestLoginFromProductPage():
 #     @pytest.fixture(scope="function", autouse=True)
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,11 @@
 from pages.product_page import ProductPage
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
+from pages.basket_page import BasketPage
 import pytest
 import time
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('link2', ["promo=offer0",
                                    "promo=offer1",
                                    "promo=offer2",
@@ -11,7 +13,6 @@ import time
                                    "promo=offer4",
                                    "promo=offer5",
                                    "promo=offer6",
-                                   "promo=offer7",
                                    pytest.param("promo=offer7", marks=pytest.mark.xfail),
                                    "promo=offer8",
                                    "promo=offer9"])
@@ -57,12 +58,22 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket_without_items()
+    basket_page.should_be_empty_basket_check_with_text()
 
 
 
@@ -76,7 +87,7 @@ class TestUserAddToBasketFromProductPage():
         password = str(time.time())
         log_page.register_new_user(email=email, password=password)
         log_page.should_be_authorized_user()
-
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         browser.delete_all_cookies()
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/neuromancer_13/"
